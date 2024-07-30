0eb31500b3442b59e0464c4e392904e658cec26b
thalissongoncalves <thalissongsilva70@gmail.com>
2024-07-30 17:47:57 -0300

    .

--- Week-4-Libraries/name.py
+++ Week-4-Libraries/name.py
@@ -1,11 +1,7 @@
 import sys
 
-# Serve para no prompt já passar, capturar e retornar o valor digitado,
-#  caso não passar vai retornar o erro de que está faltando argumentos
-
 if len(sys.argv) < 2:
     print("Too few arguments")
-elif len(sys.argv) > 2:
-    print("Too many arguments")
-else:
-    print("hello, my name is", sys.argv[1])
+
+for arg in sys.argv[1:-1]:
+    print("hello, my name is", arg)
