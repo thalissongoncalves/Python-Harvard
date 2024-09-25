54f542f205da789f862b4a9a3f2f54488c353c1d
thalissongoncalves <thalissongsilva70@gmail.com>
2024-09-25 14:31:11 -0300

    Terminado as aulas e exercícios da semana 5 Unit Tests

--- Week-5-Unit-Tests/test_calculator.py
+++ Week-5-Unit-Tests/test_calculator.py
@@ -1,16 +1,21 @@
 from calculator import square
+import pytest
 
 
-def main():
-    test_square()
+def test_positive():
+    assert square(2) == 4   
+    assert square(3) == 9
 
 
-def test_square():
-    if square(2) != 4:
-        print("2 squared was not 4")
-    if square(3) != 9:
-        print("3 squared was not 9")
+def test_negative():    
+    assert square(-2) == 4    
+    assert square(-3) == 9
 
 
-if __name__ == "__main__":
-    main()
+def test_zero():
+    assert square(0) == 0
+
+
+def test_str():
+    with pytest.raises(TypeError):
+        square("cat")
