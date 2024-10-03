3a92d34801b2656053d475684c26b96dd18684f7
thalissongoncalves <thalissongsilva70@gmail.com>
2024-10-03 13:00:34 -0300

    Semana 6 finalizada as aulas

--- Week-6-File-I-O/names.py
+++ Week-6-File-I-O/names.py
@@ -1,4 +1,22 @@
-name = input("What's your name? ")
+# Adicionar valor no arquivo
+# name = input("What's your name? ")
 
-with open("names.txt", "a") as file:
-    file.write(f"{name}\n")
+# with open("names.txt", "a") as file:
+#     file.write(f"{name}\n")
+
+
+# Ler o arquivo
+# with open("names.txt", "r") as file:
+#     for line in file:
+#         print("Hello,", line.rstrip())
+
+
+# Ler o arquivo colocando os valores em ordem alfabética
+names = []
+
+with open("names.txt") as file:
+    for line in file:
+        names.append(line.rstrip())
+
+for name in sorted(names, reverse=True):
+    print(f"hello, {name}")
