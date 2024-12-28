abe87b00a5335a6156cf2492c2fddb63236af479
thalissongoncalves <thalissongsilva70@gmail.com>
2024-12-27 23:17:06 -0300

    Terminando a aula da semana 8

--- Week-2-Loops/hogwarts.py
+++ Week-2-Loops/hogwarts.py
@@ -1,4 +1,9 @@
-students = ["Hermione", "Harry", "Ron"]
+students = [
+    {"name": "Hermione", "house": "Gryffindor", "patronus": "Otter"},
+    {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
+    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russell terrier"},
+    {"name": "Draco", "house": "Slytherin", "patronus": None}
+]
 
-for i in range(len(students)):
-    print(i, students[i])
+for student in students:
+    print(student["name"], student["house"], student["patronus"], sep=", ")
--- Week-8-Object-Oriented-Programming/student.py
+++ Week-8-Object-Oriented-Programming/student.py
@@ -1,16 +1,40 @@
 class Student:
-    ...
+    def __init__(self, name, house):
+        self.name = name
+        self.house = house
+
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if not name:
+            raise ValueError("Missing name")
+        self._name = name
+
+    @property
+    def house(self):
+        return self._house
+
+    @house.setter
+    def house(self, house):
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self._house = house
 
 def main():
     student = get_student()
-    print(f"{student.name} from {student.house}")
+    print(student)
 
 
 def get_student():
-    student = Student()
-    student.name = input("Name: ")
-    student.house = input("House: ")
-    return student
+    name = input("Name: ")
+    house = input("House: ")
+    return Student(name, house)
 
 
 if __name__ == "__main__":
