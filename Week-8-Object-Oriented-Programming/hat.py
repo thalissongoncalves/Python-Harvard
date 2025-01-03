d01d7641deabeaa36ee0a9946f9c71e10c5f8f5e
Thalisson Gonçalves <thalissongsilva70@gmail.com>
2025-01-03 13:01:23 -0300

    Codando junto com o professor da aula da semana 8

--- Week-8-Object-Oriented-Programming/hat.py
+++ Week-8-Object-Oriented-Programming/hat.py
@@ -2,11 +2,10 @@ import random
 
 
 class Hat:
-    def __init__(self):
-        self.houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+    houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
 
-    def sort(self, name):
-        print(name, "is in", random.choice(self.houses))
+    @classmethod
+    def sort(cls, name):
+        print(name, "is in", random.choice(cls.houses))
 
-hat = Hat()
-hat.sort("Harry")
+Hat.sort("Harry")
--- Week-8-Object-Oriented-Programming/student.py
+++ Week-8-Object-Oriented-Programming/student.py
@@ -6,35 +6,15 @@ class Student:
     def __str__(self):
         return f"{self.name} from {self.house}"
 
-    @property
-    def name(self):
-        return self._name
-
-    @name.setter
-    def name(self, name):
-        if not name:
-            raise ValueError("Missing name")
-        self._name = name
-
-    @property
-    def house(self):
-        return self._house
-
-    @house.setter
-    def house(self, house):
-        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
-            raise ValueError("Invalid house")
-        self._house = house
+    @classmethod
+    def get(cls):
+        name = input("Name: ")
+        house = input("House: ")
+        return cls(name, house)
 
 def main():
-    student = get_student()
+    student = Student.get()
     print(student)
-
-
-def get_student():
-    name = input("Name: ")
-    house = input("House: ")
-    return Student(name, house)
 
 
 if __name__ == "__main__":
